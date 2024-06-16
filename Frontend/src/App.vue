aef539331428ba045bb6d23b3e9514266aa0e58a
ABDALLA GAAFAR <abdullahmohmed34@gmail.com>
2024-06-16 12:35:30 +0800

    Remove unnecessary files and directories

---
+++ Frontend/src/App.vue
@@ -0,0 +1,21 @@
+// src/App.vue
+<template>
+  <div id="app">
+    <AppEvents />
+  </div>
+</template>
+
+<script>
+import AppEvents from './components/AppEvents.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppEvents
+  }
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
