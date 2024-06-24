8aa4762a04ca5962a79505d82ed76c3a58b77c09
GaafarDev <abdullahmohmed34@gmail.com>
2024-06-24 21:00:04 +0800

    Update vue.config.js for production deployment and backend proxy configuration

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  publicPath: './',
+  outputDir: 'public',
+  devServer: {
+    proxy: 'https://invite-system-backend.up.railway.app'
+  }
+}
