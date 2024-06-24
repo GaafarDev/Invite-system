563a6ad93892e222031af29df777d31d0d1a02ac
GaafarDev <abdullahmohmed34@gmail.com>
2024-06-24 21:29:09 +0800

    Update CORS configuration to allow requests from a single origin

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -1,15 +1,16 @@
 <?php
 // config/cors.php
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['invite-system-backend.up.railway.app', 'https://invite-system.netlify.app'],
+    'allowed_origins' => ['https://invite-system.netlify.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
+
 
 
 
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,27 +1,11 @@
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
-
 const apiClient = axios.create({
-  baseURL: 'https://invite-system-backend.up.railway.app/api',  // Updated URL
+  baseURL: 'https://invite-system-backend.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   }
 });
-
-// Add a request interceptor
-apiClient.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
 
 export default apiClient;
